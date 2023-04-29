03999490311a8a95fe43a64b3a3f4c2414b712a5
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-04-29 18:40:24 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-// get the input element by its ID
 let input = document.getElementById("input");
 
-
-// function that adds the passed value to the input value
 let inputValue = value => {
     input.value += value;
 };
 
-
-// function that removes the last character from the input value
 let clearInput = () => {
     input.value = input.value.slice(0, input.value.length - 1);
 };
 
-
-// function that clears the input value
 let clearAll = () => {
     input.value = "";
 };
 
-
-// function that evaluates the input value and sets the result as the new input value
 let calculateResult = () => {
     input.value = eval(input.value);
 };
+
+let numPad = event => {
+        if (event.key >= "0" && event.key <= "9" || event.key === "." || event.key === "+" || event.key === "-" || event.key === "*" || event.key === "/") {
+            inputValue(event.key);
+            console.log(input.value);
+        } 
+        if (event.key === "Enter") {
+            calculateResult();
+            console.log(input.value);
+        }
+}
+
+document.addEventListener("keyup", numPad);
